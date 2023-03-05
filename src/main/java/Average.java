dcf80e8a506665d7b2175f10192bbbec37bd3c47
dodofo1996 <dodofo1996@gmail.com>
2023-03-05 14:50:45 +0000

    average of array

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -17,11 +17,11 @@ public class Average {
      * @param nums an array to be manipulated.
      * @return the average of all the values of nums.
      */
-    public double getAvg(int[] nums, int n){
+    public double getAvg(int[] nums){
         int sum=0;
        for (int i=0;i<nums.length;i++)
         sum+=nums[i];
-        double average= (double) sum/2;
+        double average= (double) sum/nums.length;
 
         
 
